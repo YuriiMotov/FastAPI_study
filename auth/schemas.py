eee3d60a8364741d3ad26e4fbe702b4ef8719dd5
Yurii <motovyuri@ya.ru>
2023-10-06 10:50:19 +0200

    Adding the possibility to change user's role_id

--- auth/schemas.py
+++ auth/schemas.py
@@ -31,6 +31,18 @@ class UserUpdate(schemas.BaseUserUpdate):
     # fields from BaseUserUpdate
     password: Optional[str] = None
     email: Optional[str] = None
+    is_active: Optional[bool] = None
+    is_superuser: Optional[bool] = None
+    is_verified: Optional[bool] = None
 
     # additional fields
-    username: str
+    username: Optional[str] = None
+    role_id: Optional[int] = None
+
+    # add logic to `create_update_dict` method to prevent user change their own `role_id`.
+    # superuser still will be able to change it
+    def create_update_dict(self):
+        data = super().create_update_dict()
+        if 'role_id' in data:
+            data.pop('role_id')
+        return data
