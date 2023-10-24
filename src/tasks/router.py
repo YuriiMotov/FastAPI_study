f0d87c3ef0b8a7d5362a64633a030fb45825b5fb
Yurii <motovyuri@ya.ru>
2023-10-24 11:38:21 +0200

    Prioritization using separate queues

--- src/tasks/router.py
+++ src/tasks/router.py
@@ -6,23 +6,30 @@ router = APIRouter()
 
 
 @router.post(
-    '/celery-tasks-with-priorities-redis',
+    '/celery-tasks-with-priorities-queues',
     status_code=202
 )
-async def run_celery_tasks_with_priorities_redis():
-    # Run 15 exemplars of task 1 with low priority
+async def run_celery_tasks_with_priorities_queues():
+    # Run 15 instances of task 1 with default priority
     for _ in range(15):
-        t = celery_task1.apply_async(priority=2)
+        t = celery_task1.apply_async()
         t.forget()
-    # Run exemplar of task 2 with low priority
-    t = celery_task2.apply_async(priority=2)
+    # Run instance of task 2 with hight priority (set in task's declaration)
+    t = celery_task2.apply_async()
     t.forget()
-    # Run exemplar of task 3 with hight priority
-    t = celery_task3.apply_async(priority=0)
+    # Run instance of task 3 with hight priority
+    t = celery_task3.apply_async(queue='hight_priority')
     t.forget()
 
-    # Expected result: task_3 will be executed before task_2, task_2 will be executed
-    # after all instances of task_1
-    
-    # !! It's important to run worker with '--prefetch-multiplier=1' option. Otherwise 
+    # Important!
+    # To make priorities work you should run at least 2 workers and one of them should
+    # process only prioritized tasks:
+    # celery worker -A tasks.tasks:celery -Q default,hight_priority --prefetch-multiplier=1
+    # celery worker -A tasks.tasks:celery -Q hight_priority --prefetch-multiplier=1
+
+    # It's also important to run worker with '--prefetch-multiplier=1' option. Otherwise 
     # Celery will preload (CPU_cores_count)*4 tasks from the queue by one request.
+    
+    # Expected result: all the instances of task_1 will be executed by worker_1.
+    # Instances of task_3 and task 2 will be executed by worker_2.
+    
--- src/tasks/tasks.py
+++ src/tasks/tasks.py
@@ -1,25 +1,32 @@
 from time import sleep
 
 from celery import Celery
+from kombu import Exchange, Queue
+
+
+default_exchange = Exchange('default', type='direct')
+hight_priority_exchange = Exchange('hight_priority', type='direct')
 
 
 celery = Celery(
     'tasks', broker='redis://localhost:6379', backend='redis://localhost:6379',
     result_extended=True,
-    broker_transport_options={
-        'priority_steps': list(range(3)),
-        'sep': ':',
-        'queue_order_strategy': 'priority'
-    }
+    task_queues=(
+        Queue('default', default_exchange, routing_key='default'),
+        Queue('hight_priority', hight_priority_exchange, routing_key='hight_priority'),
+    ),
+    task_default_queue='default',
+    task_default_exchange='default',
+    task_default_routing_key='default'
 )
-
 
 
 @celery.task()
 def celery_task1():
     sleep(1)
 
-@celery.task()
+
+@celery.task(queue='hight_priority')
 def celery_task2():
     sleep(1)
 
